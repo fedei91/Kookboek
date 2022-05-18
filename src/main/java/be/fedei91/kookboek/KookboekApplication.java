fd29d76120e00e95ca4920d36927414e7033b8f7
Federico <fedej.izaguirre@gmail.com>
2022-05-18 15:27:13 +0200

    configuratie bestanden toegevoegd

---
+++ src/main/java/be/fedei91/kookboek/KookboekApplication.java
@@ -0,0 +1,13 @@
+package be.fedei91.kookboek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KookboekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KookboekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/fedei91/kookboek/KookboekApplicationTests.java
@@ -0,0 +1,13 @@
+package be.fedei91.kookboek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KookboekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
