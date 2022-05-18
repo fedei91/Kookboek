c6f0128cfe2f24656655b9fc6f0b586ce1bb3766
Federico <fedej.izaguirre@gmail.com>
2022-05-18 17:55:50 +0200

    GET Recept endpoint toegevoegd

--- src/main/java/be/fedei91/kookboek/restcontrollers/ReceptController.java
+++ src/main/java/be/fedei91/kookboek/restcontrollers/ReceptController.java
@@ -1,6 +1,9 @@
 package be.fedei91.kookboek.restcontrollers;
 
+import be.fedei91.kookboek.domain.Recept;
 import be.fedei91.kookboek.services.ReceptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,8 @@ class ReceptController {
         this.receptService = receptService;
     }
 
-
+    @GetMapping("{id}")
+    Recept get(@PathVariable long id) {
+        return receptService.findById(id).get();
+    }
 }
