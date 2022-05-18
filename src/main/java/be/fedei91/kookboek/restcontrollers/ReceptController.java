6340ffc1a4f9498d9d50de34941e809e70ea4db6
Federico <fedej.izaguirre@gmail.com>
2022-05-19 00:07:32 +0200

    HATEOAS location in create methode toegevoegd

--- src/main/java/be/fedei91/kookboek/restcontrollers/ReceptController.java
+++ src/main/java/be/fedei91/kookboek/restcontrollers/ReceptController.java
@@ -3,6 +3,10 @@ package be.fedei91.kookboek.restcontrollers;
 import be.fedei91.kookboek.domain.Recept;
 import be.fedei91.kookboek.exceptions.ReceptNietGevondenException;
 import be.fedei91.kookboek.services.ReceptService;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.hateoas.server.TypedEntityLinks;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +14,14 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/recepten")
+@ExposesResourceFor(Recept.class)
 class ReceptController {
     private final ReceptService receptService;
+    private final TypedEntityLinks.ExtendedTypedEntityLinks<Recept> links;
 
-    ReceptController(ReceptService receptService) {
+    ReceptController(ReceptService receptService, EntityLinks links) {
         this.receptService = receptService;
+        this.links = links.forType(Recept.class, Recept::getId);
     }
 
     @GetMapping("{id}")
@@ -33,8 +40,12 @@ class ReceptController {
     }
 
     @PostMapping
-    void post(@RequestBody Recept recept) {
+    @ResponseStatus(HttpStatus.CREATED)
+    HttpHeaders create(@RequestBody @Valid Recept recept) {
         receptService.create(recept);
+        var headers = new HttpHeaders();
+        headers.setLocation(links.linkToItemResource(recept).toUri());
+        return headers;
     }
 
     @PutMapping("{id}")
