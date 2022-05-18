b52d46125573dfa0ff6f01714d3bcfa570ed2f9c
Federico <fedej.izaguirre@gmail.com>
2022-05-18 16:04:25 +0200

    CRUD methodes gemaakt

--- src/main/java/be/fedei91/kookboek/services/ReceptService.java
+++ src/main/java/be/fedei91/kookboek/services/ReceptService.java
@@ -2,7 +2,13 @@ package be.fedei91.kookboek.services;
 
 import be.fedei91.kookboek.domain.Recept;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReceptService {
+    Optional<Recept> findById(long id);
+    List<Recept> findAll();
+    void create(Recept recept);
+    void update(Recept recept);
+    void delete(long id);
 }
