81645b2381288003412ffaaea029f3688c6fd584
Federico <fedej.izaguirre@gmail.com>
2022-05-18 17:10:59 +0200

    Naam en Instructies in Recept toegevoegd

--- src/main/java/be/fedei91/kookboek/domain/Recept.java
+++ src/main/java/be/fedei91/kookboek/domain/Recept.java
@@ -8,12 +8,14 @@ public class Recept {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String naam;
     private String ingredienten;
-    private String beschrijving;
+    private String instructies;
 
-    public Recept(String ingredienten, String beschrijving) {
+    public Recept(String naam, String ingredienten, String instructies) {
+        this.naam = naam;
         this.ingredienten = ingredienten;
-        this.beschrijving = beschrijving;
+        this.instructies = instructies;
     }
 
     protected Recept() {}
@@ -26,6 +28,10 @@ public class Recept {
         this.id = id;
     }
 
+    public String getNaam() { return naam; }
+
+    public void setNaam(String naam) { this.naam = naam; }
+
     public String getIngredienten() {
         return ingredienten;
     }
@@ -34,11 +40,11 @@ public class Recept {
         this.ingredienten = ingredienten;
     }
 
-    public String getBeschrijving() {
-        return beschrijving;
+    public String getInstructies() {
+        return instructies;
     }
 
-    public void setBeschrijving(String beschrijving) {
-        this.beschrijving = beschrijving;
+    public void setInstructies(String instructies) {
+        this.instructies = instructies;
     }
 }
